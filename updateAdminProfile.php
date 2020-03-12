be536d31fed41121e25ae0393bd20a5a578314eb
RickyTan <49142261+rickytan@users.noreply.github.com>
2020-03-13 03:17:32 +0800

    FIXED: If no fill password don't update password

--- updateAdminProfile.php
+++ updateAdminProfile.php
@@ -2,10 +2,15 @@
 include("conn.php");
 
 $username = $_REQUEST["username"];
-$password = md5($_REQUEST["password"]);
 $fullname = $_REQUEST["fullname"];
 
-$sql_update_users = "UPDATE users SET password='$password', fullname='$fullname' WHERE username='$username'";
+if(strlen($_REQUEST["password"])>0){
+  $password = md5($_REQUEST["password"]);
+  $sql_update_users = "UPDATE users SET password='$password', fullname='$fullname' WHERE username='$username'";
+}else{
+  $sql_update_users = "UPDATE users SET fullname='$fullname' WHERE username='$username'";
+}
+
 
 $conn->query($sql_update_users);
 ?>
